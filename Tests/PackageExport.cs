0faf03c3fdcc2a6537e90f8bfe4c06604924d825
Felix Herbst <felix.herbst@gmail.com>
2021-05-25 17:17:15 +0200

    added tests against GUID duplication and other cases of corrupt packages

--- Tests/PackageExport.cs
+++ Tests/PackageExport.cs
@@ -2,66 +2,193 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Needle.PackageTools;
 using NUnit.Framework;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class PackageExport
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void PackageExportSimple()
-    {
-        // Use the Assert class to test conditions
-    }
-
-    [System.Serializable]
+    [Serializable]
     class PackageData
     {
         public string name = "com.my.fake.package";
         public string version = "1.0.0";
         public string displayName = "Fake Package";
     }
+
+    string GetPackageName([CallerMemberName] string memberName = "")
+    {
+        return memberName + ".unitypackage";
+    }
+
+    private string TemporaryPackageDirectory => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Unity/AssetStoreTools/Export";
+    private string LocalPackageDirectory => "Packages/com.my.fake.package/";
+    
+    void PreparePackage(out string localDataDirectory)
+    {
+        Directory.CreateDirectory(LocalPackageDirectory);
+        File.WriteAllText(LocalPackageDirectory + "package.json", JsonUtility.ToJson(new PackageData(), true));
+
+        localDataDirectory = LocalPackageDirectory + "data/";
+        Directory.CreateDirectory(localDataDirectory);
+    }
+
+    void CreatePackage(string temporaryPackageDir, string outputFileName)
+    {
+        if (!Zipper.TryCreateTgz(temporaryPackageDir, outputFileName))
+        {
+            UnityEngine.Profiling.Profiler.EndSample();
+            throw new Exception("Failed creating .unitypackage " + outputFileName);
+        }
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Debug.Log("Tear Down Called");
+        Directory.Delete(TemporaryPackageDirectory, true);
+        Directory.Delete(LocalPackageDirectory, true);
+    }
     
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator PackageExportAndImport()
+    public IEnumerator FilesHaveSameExistingGuids_ShouldFail()
     {
-		var dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Unity/AssetStoreTools/Export";
-        var fileName = "tempPackage.unitypackage";
-
-        // create fake package
-        var root = "Packages/com.my.fake.package/";
-        Directory.CreateDirectory(root);
-        File.WriteAllText(root + "package.json", JsonUtility.ToJson(new PackageData(), true));
-
-        var dataDir = root + "data/";
-        Directory.CreateDirectory(dataDir);
+        var fileName = GetPackageName();
+        PreparePackage(out var dataDir);
         
-        // create many fake files without meta information
-        for (int i = 0; i < 10000; i++)
+        var guidToFile = new Dictionary<string, string>();
+
+        var guid = Guid.NewGuid();
+
+        try
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                var file = $"{dataDir}file_{i}.txt";
+                // unique content
+                File.WriteAllText(file, $"content:{i}");
+                // create meta
+                File.WriteAllText(file + ".meta", "guid: " + guid.ToString("N"));
+                UnitypackageExporter.AddToUnityPackage(file, TemporaryPackageDirectory, ref guidToFile);
+            }
+        }
+        catch (ArgumentException)
+        {
+            Assert.Pass("Exception was thrown when adding duplicate GUID");
+        }
+        
+        Assert.Fail("We shouldn't get here!");
+        
+        CreatePackage(TemporaryPackageDirectory, fileName);
+        
+		yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator FilesHaveSameContent()
+    {
+        var fileName = GetPackageName();
+        PreparePackage(out var dataDir);
+        
+        var guidToFile = new Dictionary<string, string>();
+        
+        for (int i = 0; i < 23; i++)
         {
             var file = $"{dataDir}file_{i}.txt";
-            File.WriteAllText(file, $"content:{i}");
-            UnitypackageExporter.AddToUnityPackage(file, dir);
+            File.WriteAllText(file, $"content:{i % 4}"); // some files have the same content
+            UnitypackageExporter.AddToUnityPackage(file, TemporaryPackageDirectory, ref guidToFile);
         }
         
-        // create fake meta ?
+        CreatePackage(TemporaryPackageDirectory, fileName);
         
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator GeneratedGuidConflictsWithAssetDatabaseGuid()
+    {
+        var fileName = GetPackageName();
+        PreparePackage(out var dataDir);
         
-        // add temp files
-		
- 		if (!Zipper.TryCreateTgz(dir, fileName))
+        var guidToFile = new Dictionary<string, string>();
+
+        var existingFile = "Packages/com.needle.upm-in-unitypackage/Tests/Data/ImageWithGuidConflict.png";
+        var newFile = $"{dataDir}file_with_content_collision.txt";
+        File.WriteAllText(newFile, $"content:very-colliding-content"); // produces GUID 58147484c7ca7af8bf7d69ec3916dc90
+
+        UnitypackageExporter.AddToUnityPackage(existingFile, TemporaryPackageDirectory, ref guidToFile);
+        UnitypackageExporter.AddToUnityPackage(newFile, TemporaryPackageDirectory, ref guidToFile);
+        
+        Debug.Log(string.Join("\n", guidToFile.Select(x => x.Key + ": " + x.Value)));
+
+        CreatePackage(TemporaryPackageDirectory, fileName);
+        
+        yield return null;
+    }
+    
+        
+    [UnityTest]
+    public IEnumerator AssetDatabaseGuidAddedAfterGeneratedGuid_ShouldFail()
+    {
+        var fileName = GetPackageName();
+        PreparePackage(out var dataDir);
+        
+        var guidToFile = new Dictionary<string, string>();
+
+        var existingFile = "Packages/com.needle.upm-in-unitypackage/Tests/Data/ImageWithGuidConflict.png";
+        var newFile = $"{dataDir}file_with_content_collision.txt";
+        File.WriteAllText(newFile, $"content:very-colliding-content"); // produces GUID 58147484c7ca7af8bf7d69ec3916dc90
+
+        UnitypackageExporter.AddToUnityPackage(newFile, TemporaryPackageDirectory, ref guidToFile);
+
+        try
         {
-            UnityEngine.Profiling.Profiler.EndSample();
-            throw new Exception("Failed creating .unitypackage " + fileName);
+            UnitypackageExporter.AddToUnityPackage(existingFile, TemporaryPackageDirectory, ref guidToFile);
+        }
+        catch (ArgumentException)
+        {
+            Assert.Pass("Exception was thrown when adding duplicate GUID");
         }
         
-        EditorUtility.RevealInFinder(fileName);
-        Directory.Delete(dir, true);
-		yield return null;
+        Assert.Fail("We shouldn't get here!");
+        
+        Debug.Log(string.Join("\n", guidToFile.Select(x => x.Key + ": " + x.Value)));
+        
+        CreatePackage(TemporaryPackageDirectory, fileName);
+        
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MultipleIdenticalFilePaths_ShouldFail()
+    {
+        var fileName = GetPackageName();
+        PreparePackage(out var dataDir);
+        
+        var guidToFile = new Dictionary<string, string>();
+            
+        var newFile = $"{dataDir}file_with_content_collision.txt";
+        File.WriteAllText(newFile, $"content:very-colliding-content"); // produces GUID 58147484c7ca7af8bf7d69ec3916dc90
+        
+        UnitypackageExporter.AddToUnityPackage(newFile, TemporaryPackageDirectory, ref guidToFile);
+        try
+        {
+            UnitypackageExporter.AddToUnityPackage(newFile, TemporaryPackageDirectory, ref guidToFile);
+        }
+        catch(ArgumentException)
+        {
+            Assert.Pass("Exception was thrown when adding duplicate paths");
+        }
+        
+        Assert.Fail("We shouldn't get here!");
+        
+        CreatePackage(TemporaryPackageDirectory, fileName);
+        
+        yield return null;
     }
 }
